818bfbb6921971012703f91e52e096149463ab2b
Maciej Kolacinski <maciek200895@gmail.com>
2024-10-17 10:03:42 +0100

    using correct package on template

--- idx-template.nix
+++ idx-template.nix
@@ -4,7 +4,7 @@
   # Which nixpkgs channel to use.
   channel = "stable-24.05"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
-  packages = [ pkgs.dotnet-sdk_6 ];
+  packages = [ pkgs.dotnet-sdk_8 ];
   # Sets environment variables in the workspace
   env = { };
   idx = {
